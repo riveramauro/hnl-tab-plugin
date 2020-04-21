5408d906c685e8c002442f64d16b232d5bf5b981
Mauricio Rivera <maurocol03@gmail.com>
2020-04-21 16:22:06 -0400

    Fixes to display page

--- admin/class-hnl-tab-form-admin.php
+++ admin/class-hnl-tab-form-admin.php
@@ -121,23 +121,6 @@ class Hnl_Tab_Form_Admin {
   }
 
   /**
-   * Add settings action link to the plugins page.
-   *
-   * @since    1.0.0
-   */
-
-  public function add_action_links( $links ) {
-    /**
-    *  Documentation : https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
-    */
-    $settings_link = array(
-      '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
-    );
-    return array_merge(  $settings_link, $links );
-
-  }
-
-  /**
    * Render the settings page for this plugin.
    *
    * @since    1.0.0
--- includes/class-hnl-tab-form.php
+++ includes/class-hnl-tab-form.php
@@ -160,9 +160,9 @@ class Hnl_Tab_Form {
     // Add menu items
     $this->loader->add_action( 'admin_menu', $plugin_name, 'add_plugin_admin_menu');
     // Add Settings link to the plugin
-    $plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
-    $this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
-    
+    // $plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
+    // $this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
+
 	}
 
 	/**
